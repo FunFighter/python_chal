db815e957150f7eb78ed5bf71e08d1b90f153613
FunFighter <brandon.lealwd@icloud.com>
2019-03-14 20:37:56 -0700

    PyPoll main filled in w/ text output

--- PyPoll/.ipynb_checkpoints/main-checkpoint.py
+++ PyPoll/.ipynb_checkpoints/main-checkpoint.py
@@ -3,19 +3,19 @@ import pandas as pd
 
 csv_voter_data = pd.read_csv("../Resources/election_data.csv")
 
-def Analyze_Data(voter_data):
+def Analyze_Data(voter_data)
     #total number of votes
     total_votes = len(voter_data.index)
     
     #list of candidates unique
     unique_candidates = voter_data["Candidate"].unique()
-    #this goes over the percent each because it needs to be defined first to use
-    #votes per candidate
-    vote_count = voter_data['Candidate'].value_counts()
-
+    
     #percent each candidate
     pct_count = vote_count/total_votes * 100
 
+    #votes per candidate
+    vote_count = voter_data['Candidate'].value_counts()
+    
     #popular vote winner
     popular_vote = vote_count[:1]
     
@@ -31,5 +31,3 @@ def Analyze_Data(voter_data):
         print(f"Total Percent: {pct_count}", file=text_file)
         print(f"Vote Count: {vote_count}", file=text_file)
         print(f"Popular Vote: {popular_vote}", file=text_file)
-
-Analyze_Data(csv_voter_data)
