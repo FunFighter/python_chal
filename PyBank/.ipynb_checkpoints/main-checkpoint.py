b7e9560124b0a1b4af28beb7b85eecc17fe477d8
FunFighter <brandon.lealwd@icloud.com>
2019-03-14 17:47:14 -0700

    added print to txt

--- PyBank/.ipynb_checkpoints/main-checkpoint.py
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -1,17 +1,18 @@
 import numpy as np
 import pandas as pd
 
-budget = pd.read_csv("../Resources/budget_data.csv",parse_dates=True)
+csv_budget = pd.read_csv("../Resources/budget_data.csv",parse_dates=True)
 
 def currency(amount):
     if amount >= 0:
+        #str $+numbers.2 dec places 
         return '${:,.2f}'.format(amount)
     else:
         return '-${:,.2f}'.format(-amount)
 
-#[0] = date [1]= gain/loss
+#[0] = date, [1]= gain/loss
 def Financial_Analysis(budget):
-    #proloss = budget.iloc[:, 1]
+
     date = budget["Date"]
     proloss = budget["Profit/Losses"]
     
@@ -30,17 +31,18 @@ def Financial_Analysis(budget):
     #greatestloss
     greatest_loss = proloss.min()
 
+
+    print(f"Total Months: {total_months}")
+    print(f"Total: {currency(total_returns)}")
+    print(f"Average Change: {currency(avg_changes)}")
+    print(f"Greatest Increase: {currency(greatest_profit)}")
+    print(f"Greatest Decrease: {currency(greatest_loss)}")
+    
+with open("Output.txt", "w") as text_file:
     print(f"Total Months: {total_months}")
     print(f"Total: {currency(total_returns)}")
     print(f"Average Change: {currency(avg_changes)}")
     print(f"Greatest Increase: {currency(greatest_profit)}")
     print(f"Greatest Decrease: {currency(greatest_loss)}")
 
-with open(csvpath, 'r') as csvfile:
-
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    header = next(csvreader)
-
-    for row in csvreader:
-       Financial_Analysis(row)
+Financial_Analysis(csv_budget)
--- PyBank/main.py
+++ PyBank/main.py
@@ -5,11 +5,12 @@ csv_budget = pd.read_csv("../Resources/budget_data.csv",parse_dates=True)
 
 def currency(amount):
     if amount >= 0:
+        #str $+numbers.2 dec places 
         return '${:,.2f}'.format(amount)
     else:
         return '-${:,.2f}'.format(-amount)
 
-#[0] = date [1]= gain/loss
+#[0] = date, [1]= gain/loss
 def Financial_Analysis(budget):
 
     date = budget["Date"]
@@ -30,10 +31,19 @@ def Financial_Analysis(budget):
     #greatestloss
     greatest_loss = proloss.min()
 
+
     print(f"Total Months: {total_months}")
     print(f"Total: {currency(total_returns)}")
     print(f"Average Change: {currency(avg_changes)}")
     print(f"Greatest Increase: {currency(greatest_profit)}")
     print(f"Greatest Decrease: {currency(greatest_loss)}")
+    
+    #log to output file
+    with open("Output.txt", "w") as text_file:
+        print(f"Total Months: {total_months}", file=text_file)
+        print(f"Total: {currency(total_returns)}", file=text_file)
+        print(f"Average Change: {currency(avg_changes)}", file=text_file)
+        print(f"Greatest Increase: {currency(greatest_profit)}", file=text_file)
+        print(f"Greatest Decrease: {currency(greatest_loss)}", file=text_file)
 
 Financial_Analysis(csv_budget)
